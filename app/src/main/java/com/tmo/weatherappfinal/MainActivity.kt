a948f8a935d88f472d55fe089f22c8471adb936a
tomasmonteiro03 <76120194+tomasmonteiro03@users.noreply.github.com>
2023-10-31 23:48:21 +0000

    second commit headerPreview done

--- app/src/main/java/com/tmo/weatherappfinal/MainActivity.kt
+++ app/src/main/java/com/tmo/weatherappfinal/MainActivity.kt
@@ -3,13 +3,28 @@ package com.tmo.weatherappfinal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tmo.weatherappfinal.ui.theme.WeatherAppFinalTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +37,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true, widthDp = 390)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun HeaderPreview(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
         modifier = modifier
-    )
-}
+            .fillMaxWidth()
+            .padding(vertical = 15.dp, horizontal = 16.dp)
+    ) {
+        Button(
+            onClick = {/*TODO*/},
+            contentPadding = PaddingValues(),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            modifier = modifier
+                .size(38.dp)
+                .clip(CircleShape)
+        )
+        {
+            Image(painter = painterResource(id = R.drawable.search), contentDescription = null)
+        }
+        Button(
+            onClick = {/*TODO*/},
+            contentPadding = PaddingValues(),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            modifier = modifier
+                .size(38.dp)
+                .clip(CircleShape)
+        )
+        {
+            Image(painter = painterResource(id = R.drawable.foto), contentDescription = null)
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAppFinalTheme {
-        Greeting("Android")
     }
 }
