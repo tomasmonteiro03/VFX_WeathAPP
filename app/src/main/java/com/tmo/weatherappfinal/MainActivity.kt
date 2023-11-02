f4b8911cd060ed38c30ec7f51e0522c02958e999
tomasmonteiro03 <76120194+tomasmonteiro03@users.noreply.github.com>
2023-11-02 19:45:10 +0000

    criação de data e nome

--- app/src/main/java/com/tmo/weatherappfinal/MainActivity.kt
+++ app/src/main/java/com/tmo/weatherappfinal/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,9 +24,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.tmo.weatherappfinal.ui.theme.WeatherAppFinalTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,12 +45,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MainApp()
                 }
             }
         }
     }
 }
+
+@Preview(showBackground = true, widthDp = 390)
+@Composable
+fun MainApp(){
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(vertical = 5.dp)){
+        HeaderPreview()
+        BoasVindasUser()
+    }
+
+}
+
+
 
 @Preview(showBackground = true, widthDp = 390)
 @Composable
@@ -74,8 +96,49 @@ fun HeaderPreview(modifier: Modifier = Modifier) {
                 .clip(CircleShape)
         )
         {
-            Image(painter = painterResource(id = R.drawable.foto), contentDescription = null)
+            Image(painter = painterResource(id = R.drawable.batman), contentDescription = null)
         }
 
     }
+}
+
+@Preview(showBackground = true, widthDp = 390)
+@Composable
+fun BoasVindasUser(username: String= "Tomás")
+{
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp)) {
+
+        Row(modifier = Modifier.fillMaxWidth())  {
+            Text("Olá",
+                fontSize= TextUnit(25f, TextUnitType.Sp ),
+                color= MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Light )
+            Text(" $username",
+                fontWeight = FontWeight.Bold,
+                color= MaterialTheme.colorScheme.primary,
+                fontSize= TextUnit(25f, TextUnitType.Sp)
+            )
+        }
+        Row(modifier = Modifier.fillMaxWidth())  {
+            Text(formatData(Calendar.getInstance().time),
+                fontSize= TextUnit(18f, TextUnitType.Sp ),
+                fontWeight = FontWeight.Light,
+                color = MaterialTheme.colorScheme.secondary)
+        }
+        Row(modifier = Modifier.fillMaxWidth())  {
+            Text("Glória do Ribatejo",
+                fontSize= TextUnit(18f, TextUnitType.Sp ),
+                fontWeight = FontWeight.Light,
+                color = MaterialTheme.colorScheme.secondary)
+        }
+    }
+}
+
+
+// Helpers
+fun formatData(data: Date): String {
+    val format = SimpleDateFormat("dd MMMM, EEEE yyyy", Locale("pt", "PT"))
+    return format.format(data)
 }
