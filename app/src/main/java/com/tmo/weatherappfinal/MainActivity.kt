f23e48707ae6d5d5d20bb95274fdf6d37e371a30
tomasmonteiro03 <76120194+tomasmonteiro03@users.noreply.github.com>
2023-11-02 20:41:21 +0000

    commit imagemcentral

--- app/src/main/java/com/tmo/weatherappfinal/MainActivity.kt
+++ app/src/main/java/com/tmo/weatherappfinal/MainActivity.kt
@@ -3,13 +3,17 @@ package com.tmo.weatherappfinal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -22,6 +26,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -60,6 +66,7 @@ fun MainApp(){
         .padding(vertical = 5.dp)){
         HeaderPreview()
         BoasVindasUser()
+        ImagemCentro()
     }
 
 }
@@ -136,8 +143,71 @@ fun BoasVindasUser(username: String= "Tomás")
     }
 }
 
+@Preview(showBackground = true, widthDp = 390)
+@Composable
+fun ImagemCentro(temperatura: Int = 23, estadoTempo: String = "Limpo", @DrawableRes iconResource: Int= R.drawable.sun)
+{
+    Row(modifier =
+    Modifier
+        .padding(vertical = 25.dp)
+        .height(300.dp)
+        .fillMaxWidth()
+        .background(color = Color.Transparent),
+        Arrangement.Center) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .size(250.dp)
+                .shadow(80.dp, CircleShape, spotColor = Color.Transparent)
+                .clip(CircleShape)
+                .background(
+                    Brush.verticalGradient(
+                        colorStops = arrayOf(
+                            Pair(0.2f, MaterialTheme.colorScheme.secondaryContainer),
+                            Pair(1f, MaterialTheme.colorScheme.tertiaryContainer)
+                        )
+                    )
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Box(contentAlignment = Alignment.TopCenter, modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 15.dp)){
+                Box( modifier = Modifier
+                    .height(140.dp)
+                    .padding(top = 0.dp)){
+                    Image(
+                        painter = painterResource(id = iconResource),
+                        contentDescription = null
+                    )
+                }
+                Box(
+                    contentAlignment = Alignment.CenterEnd,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 90.dp)){
+                    Text("$temperatura"+"º",
+                        modifier = Modifier.padding(0.dp,0.dp,100.dp, 0.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary,
+                        fontSize= TextUnit(55f, TextUnitType.Sp))
+                }
+                Box(
+                    contentAlignment = Alignment.CenterStart,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 90.dp)){
+                    Text("$estadoTempo",
+                        fontWeight = FontWeight.ExtraLight,color= MaterialTheme.colorScheme.primary,
+                        fontSize= TextUnit(15f, TextUnitType.Sp),
+                        modifier = Modifier.padding(145.dp,23.dp,0.dp, 0.dp))
+                }
+            }
+        }
+    }
+}
 
-// Helpers
 fun formatData(data: Date): String {
     val format = SimpleDateFormat("dd MMMM, EEEE yyyy", Locale("pt", "PT"))
     return format.format(data)
